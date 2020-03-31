f56f987c8616956717cbb3eeb8b3265b8d10c3da
LaupWing <loc_strict@hotmail.com>
2020-03-31 12:53:45 +0200

    15 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Card(),
-
-));
+      home: Card(),
+    ));
 
 class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              )
-            ),
-            SizedBox(height: 10.0,),
-            Text(
-                'Laup-Wing',
-                style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('ID Card'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/naruto2.jpg'),
+                    radius: 40.0,
+                  ),
+                ),
+                Divider(
+                  height: 60.0,
+                  color: Colors.grey[800],
+                ),
+                Text('NAME',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      letterSpacing: 2.0,
+                    )),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text('Laup-Wing',
+                    style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 2.0,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(height: 30.0),
+                Text('Level',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      letterSpacing: 2.0,
+                    )),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text('9',
+                    style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 2.0,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(height: 30.0),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'laupwing@hotmail.com',
+                      style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18.0,
+                        letterSpacing: 1.0,
+                      ),
+                    )
+                  ],
                 )
-            )
-          ],
-        )
-      )
-    );
+              ],
+            )));
   }
 }
